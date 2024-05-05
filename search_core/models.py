a5b17c14c1a3239d09ef6ab2a7986db6347bc175
Atieno-Ouma <atienooumavee@gmail.com>
2024-05-05 21:09:25 +0300

    ft(template-user): add model to be referenced in the by elastic search.

--- search_core/models.py
+++ search_core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255)
+    phone_no = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
+    id_no = models.CharField(max_length=255)
+    location_id = models.BigIntegerField()
+    campaign_id = models.PositiveIntegerField(blank=True, null=True)
+    level_id = models.BigIntegerField(blank=True, null=True)
+    role_id = models.IntegerField()
+    org_types_id = models.BigIntegerField()
+    email_verified_at = models.DateTimeField(blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    remember_token = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    has_smartphone = models.BooleanField(default=False)
+    job_group = models.CharField(max_length=255, blank=True, null=True)
+    status = models.BooleanField(default=True)
+    sync_status = models.BooleanField(default=False)
+    deactivated_by = models.BigIntegerField(blank=True, null=True)
